abe4651a7778dd27132238769d7799377f087af6
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-03-01 10:24:10 +0530

    Updated Solution - GfG to GitHub

--- Medium/Peak element/peak-element.cpp
+++ Medium/Peak element/peak-element.cpp
@@ -20,17 +20,22 @@ class Solution
        }
        int lo=0,hi=n-1;
        while(lo<hi){
-           int mid=lo+(hi-lo)/2;
-           if(arr[mid] < arr[mid+1]){
-               lo=mid+1;
+           int mid=(lo+hi)>>1;
+           if((mid==0 && arr[mid] >= arr[mid+1]) || (mid==n-1 && arr[mid] >= arr[mid-1]) || (mid!=0 && mid!=n-1 && arr[mid]>=arr[mid+1] && arr[mid]>=arr[mid-1])){
+               return mid;
+           }
+           else if((mid==0 && arr[mid] < arr[mid+1]) || (mid!=0 && mid!=n-1 && arr[mid]<arr[mid+1] && arr[mid]>arr[mid-1])){
+               lo = mid+1;
            }
            else{
-               hi=mid;
+               hi=mid-1;
            }
+           
        }
-       return lo;
+       return 0;
     }
 };
+
 
 //{ Driver Code Starts.
 
