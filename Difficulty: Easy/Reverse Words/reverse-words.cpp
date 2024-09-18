2eface086add7703bde28611fbae57770f125194
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-09-19 01:22:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to reverse words in a given string.
+    string reverseWords(string str) {
+        // code here
+        stringstream ss(str);
+        string s;
+        string ans="";
+        while(getline(ss,s,'.')){
+            ans=s+ans;
+            ans='.'+ans;
+        }
+        ans.erase(0,1);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.reverseWords(s) << endl;
+    }
+}
+// } Driver Code Ends
